e939cd9ed3f59db84ea47af3e6a84b825700de69
Alison Andre Monteiro da Silva <alison.andremonteiro@outlook.com>
2024-01-04 07:00:19 -0300

    feat: Layout de rotas aplicado

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 // Pages
 import { Home } from './pages/home'
 import { History } from './pages/history'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
